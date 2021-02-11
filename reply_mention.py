14810522b8e28d7f5864b079eb796b6cb7634344
sgamboa13 <sgamboa13@users.noreply.github.com>
2021-02-11 11:51:17 -0800

    Added flag recognition
    Flag recognition is based off of having a  ' - ' symbol attached to (right before) a particular character (or set of characters). A list of flags and descriptions must be developed for later use.

--- reply_mention.py
+++ reply_mention.py
@@ -10,6 +10,21 @@ class reddit_bot:
         self.mentions = self.reddit.inbox.mentions
         self.stream = praw.models.util.stream_generator(self.mentions, skip_existing=True)
 
+    def parse_flags(self, text):
+        flag_messages = ""
+        flag_list = [] #this will hold all flags that we want to use
+        flag_descriptions = [] #this will hold all flag description or names by index for the bot to respond with
+        test_list = ["ab","cd","ef","ps","a"]#list of flags for testing responses
+        #if test_list in text: # don't want to use this because we need to know exactly which flag is being used
+        for flag in test_list:
+            if flag in text:
+                loc = text.find(flag) #this implementation will only return the index of the first instance of our string
+                            #if we really want to, we can count all instance, and check all instances
+                if loc != 0 and text[loc-1] == '-':
+                    print("Flag found: -\n",flag,"")
+                    flag_messages += "\n\n\nFlag found: -" + flag
+        return flag_messages
+
     def checkout_mention(self, mention):
         parent_id = mention.parent()
         message = ""
@@ -26,7 +41,8 @@ class reddit_bot:
                 # this works with the case where the image might have some specific formatand doesn't end in .jpg...
                 image_URL = parent.url
                 images.append(parent.url)
-                if ('.jpg' or '.jpeg' or '.png' or ".jfif" in parent.selftext) and len(parent.selftext) != 0:
+
+            if ('.jpg' or '.jpeg' or '.png' or ".jfif" in parent.selftext) and len(parent.selftext) != 0:
                     '''
                 Had to account for the case where for some reason the keys would be foud in the selftext, but the
                   selftext was actually empty. Also had to work around whether or not somebody adds text to the original post.
@@ -36,14 +52,16 @@ class reddit_bot:
                     images = [] #reset images
                     num_images = parent.selftext.count("https://")  # check how many links we have total in the text
                     selftext = parent.selftext
+                    print("selftext: ",selftext)
                     print("\n***")
                     for i in range(num_images):
                         unstripped_URL = selftext.split("https://")[i+1]  # for each link, take the split section with the actual part of the link (caption removed)
                         image_URL = unstripped_URL.split(')')[0]  # strip the closing' ) ' and the rest of remaining selftext
                         if '.jpg' or '.jpeg' or '.png' or ".jfif" in image_URL:  # use this to confirm it's a link to an image, and not to another site
                             images.append(image_URL)  # add it to our list of links to print
-
-                message += "\n\nImage found! \n\nImage URL(s): " + str(images)  # a double \n, marks for a newline in reddit
+                    print(type(mention))
+                    message += "\n\nImage found! \n\nImage URL(s): " + str(images)  # a double \n, marks for a newline in reddit
+                    message += self.parse_flags(str(mention.body))
             else:
                 message += "\n\nNo Image found in post!"
 
